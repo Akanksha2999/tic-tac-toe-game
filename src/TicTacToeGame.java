bbf4980ccd4348a6eda518b0b94c95f817a78297
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-05 11:32:25 +0530

    ADDED: Displayed the tic tac toe empty game board with 9 empty spaces

---
+++ src/TicTacToeGame.java
@@ -0,0 +1,24 @@
+public class TicTacToeGame {
+    //Declared a char type array named as gameArray of size 10
+    public static char[] gameArray = new char[10];
+
+    public static void ticTacToeGame() {
+        for (int i = 0; i < gameArray.length; i++) {
+            gameArray[i] = ' ';
+        }
+    }
+
+    //Displaying the game board with 9 empty spaces
+    public static void showBoard() {
+        System.out.println(gameArray[0] + " | " + gameArray[1] + " | " + gameArray[2]);
+        System.out.println(gameArray[3] + " | " + gameArray[4] + " | " + gameArray[5]);
+        System.out.println(gameArray[6] + " | " + gameArray[7] + " | " + gameArray[8]);
+    }
+
+    //Calling ticTacToeGame and showBoard method
+    public static void main(String[] args) {
+        System.out.println("Welcome to tic tac toe game");
+        ticTacToeGame();
+        showBoard();
+    }
+}
