e3812f933dd05c9d3a9d75d6a836220cc9bfb5bd
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-15 08:05:44 +0530

    ADDED: Player can make a choice between x and 0

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -3,6 +3,10 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
     public static char[] gameBoard = new char[10];
+    public static char userTurn;
+    public static char computerTurn;
+    public static char user = '1';
+    public static Scanner scanner = new Scanner(System.in);
 
 
     public static void showBoard() {
@@ -12,11 +16,16 @@ public class TicTacToeGame {
         System.out.println(gameBoard[6] + " | " + gameBoard[7] + " | " + gameBoard[8]);
     }
 
+    public static char user() {
+        System.out.println("Enter your choice  X or O only");
+        return scanner.next().toUpperCase().charAt(0);
+    }
+
 
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe game");
         showBoard();
-
+        user();
 
     }
 }
